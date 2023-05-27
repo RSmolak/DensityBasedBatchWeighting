4709d83359004b72599dcc4a1fc9f358a71a88d2
RSmolak <rafal.smolak@gmail.com>
2023-05-27 16:57:30 +0200

    learning/testing cycle. NN doesn't learn for some reason

--- main.py
+++ main.py
@@ -1,46 +1,56 @@
-
 from sklearn.datasets import make_classification
-from sklearn.model_selection import train_test_split
 from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.base import clone
+from sklearn.metrics import accuracy_score
 
 from model import MyNNModel, DensityBasedNNClassifier
 
 import torch.nn as nn
 import torch.optim as optim
 
+import numpy as np
+
 
 # Learning hyperparameters
 num_epoch = 1000
 batch_size = 100
-learning_rate = 0.0001
+learning_rate = 0.00005
 
 # Generate a dataset
-X, y = make_classification(n_samples=1000, n_features=3, n_informative=3, n_redundant=0)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X, y = make_classification(n_samples=1000, n_features=2, n_informative=2, n_redundant=0)
+generated_dataset = (X,y)
+
 
 # Define model
-model = MyNNModel(len(X_train[0]), 1)
+model = MyNNModel(len(X[0]), 1)
 criterion = nn.MSELoss(reduction='none')
 optimizer = optim.SGD(model.parameters(), lr=learning_rate)
 
-classifier = DensityBasedNNClassifier(model, optimizer, criterion, batch_size, num_epoch)
-classifier.fit(X_train, y_train)
+dbnn_classifier = DensityBasedNNClassifier(model, optimizer, criterion, batch_size, num_epoch)
+
+DATASETS = [
+    generated_dataset,
+]
+
+CLASSIFIERS = [
+    dbnn_classifier
+]
 
 
+rskf = RepeatedStratifiedKFold(n_splits=5, n_repeats=2, random_state=42)
+scores = np.zeros(shape = (len(DATASETS), len(CLASSIFIERS), rskf.get_n_splits()))
+
+#dbnn_classifier.fit(X, y)
+for dataset_idx, (X,y) in enumerate(DATASETS):
+    for classifier_idx, clf_prot in enumerate(CLASSIFIERS):
+        for fold_idx, (train, test) in enumerate(rskf.split(X, y)):
+            clf = clone(clf_prot) # jak nie ma clone to się uczy, a jak jest to nie. Pewnie jakiś brak spójności między scikit i pytorch
+            clf.fit(X[train], y[train])
+            y_pred = clf.predict(X[test])
+            score = accuracy_score(y[test], y_pred)
+            scores[dataset_idx, classifier_idx, fold_idx] = score
 
 
-
-# Print data, weights and classes
-# for i in range(len(X)):
-#     print(X[i], weights[i], y[i])
-
-
-# colors = df["Target"].map({0: 'blue', 1: 'red'})
-#
-# plt.figure(figsize=(10, 7))
-# plt.scatter(df["Feature 1"], df["Feature 2"], c=colors, s=weights)
-# plt.xlabel('Feature 1')
-# plt.ylabel('Feature 2')
-# plt.title('Scatter plot of the dataset')
-# plt.show()
+print(scores)
+np.save("scores", scores)
 
--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 import torch.nn as nn
 import torch
-import torch.optim as optim
 from sklearn.base import BaseEstimator, ClassifierMixin
 from densityWeights import get_kde_weights
 from datasets.custom_dataset import WeightedDataset
@@ -8,14 +7,16 @@ from torch.utils.data import DataLoader
 class MyNNModel(nn.Module):
     def __init__(self, input_size, output_size):
         super(MyNNModel, self).__init__()
-        self.fc1 = nn.Linear(input_size, 20)
-        self.fc2 = nn.Linear(20, 20)
-        self.fc3 = nn.Linear(20, output_size)
+        self.fc1 = nn.Linear(input_size, 100)
+        self.fc2 = nn.Linear(100, 100)
+        self.fc3 = nn.Linear(100, 100)
+        self.fc4 = nn.Linear(100, output_size)
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = torch.relu(self.fc3(x))
+        x = self.fc4(x)
         return x
 
 class DensityBasedNNClassifier(BaseEstimator, ClassifierMixin):
@@ -26,7 +27,9 @@ class DensityBasedNNClassifier(BaseEstimator, ClassifierMixin):
         self.optimizer = optimizer
         self.num_epoch = num_epoch
 
-    def fit(self, X, y=None):
+    def fit(self, X, y):
+        X = torch.tensor(X, dtype=torch.float)
+        y = torch.tensor(y, dtype=torch.long)
         generated_dataset_weights = get_kde_weights(X)
         generated_dataset = WeightedDataset(X, y, generated_dataset_weights)
         train_generated_data_loader = DataLoader(generated_dataset, self.batch_size, shuffle=True)
@@ -42,5 +45,12 @@ class DensityBasedNNClassifier(BaseEstimator, ClassifierMixin):
                 weighted_loss.backward()
                 self.optimizer.step()
                 print(f'Epoch: {epoch + 1}, Batch: {i + 1}, Loss: {weighted_loss.item()}')
-
         return self
+
+    def predict(self, X):
+        self.model.eval()  # ustawiamy model w tryb ewaluacji
+        inputs = torch.tensor(X, dtype=torch.float)
+        with torch.no_grad():  # wyłączamy obliczanie gradientów
+            outputs = self.model(inputs)
+        _, predicted = torch.max(outputs, 1)  # znajdujemy indeks klasy o największej wartości
+        return predicted.numpy()  # zwracamy numpy array z przewidywaniami
